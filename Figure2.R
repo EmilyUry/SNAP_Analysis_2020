6bc3cb77c957e2f681033d8b6b5c6933cb24c67d
Emily Ury <ury.emily@gmail.com>
2020-10-08 11:09:05 -0400

    8 panel box plot of response vars

--- Figure2.R
+++ Figure2.R
@@ -16,6 +16,7 @@ names(x) <- c("Date", "Year", "Site", "Treatment", "Depth", "Core", "Salinity","
               "SM", "LOI", "pH", "Roots", "DOC", "TDN", 
               "Cl", "SO4", "Na", "K", "Mg", "Ca", "TIC", "TCC", "NH4", "ICNO3", "ICPO4",
               "Cmin_s", "Cmin_c", "SIR_s", "SIR_c", "Br", "Phenol", "NO3", "PO4", "Suva254")
+x$carb <- x$BD*x$LOI/100 ## grams carbon per cubic cm soil
 
 x$Yr_mo <- c(rep("2020",60), rep("2019", 60), rep("2018 May", 60), rep("2018 Jul", 60))
 x$xx <- c(rep(4,60), rep(3, 60), rep(1, 60), rep(2, 60))
@@ -29,6 +30,75 @@ x10 <- x[which(x$Depth == "(5-10)"),]
 
 ## version 1 -- normal scales
 
-par(mfrow = c(4,2), oma = c(1,7,1,1), mar = c(1,0.2,1,0.2))
+par(mfrow = c(4,2),  oma = c(3,7,3,1), mar = c(0.4,0.2,0.4,0.2))
+
+boxplot((DOC)~Treatment*xx, data = x5, col = c("#143ee3bb", "#ff9b4aee"), xlab = " ", ylab = " ", xaxt = 'n', ylim = c(0, 50),
+        at = c(1,2,4,5,7,8,10,11), las = 1)
+legend("topleft", c("Control", "Salt"), pt.bg = c("#143ee3bb", "#ff9b4aee"),  pch = 22, pt.cex = 1.7, bty = 'n', ncol = 2)
+mtext("DOC", 2, 3, outer = F, las =1, cex = 1.1)
+mtext("mg/L", 2, 3, outer = F, las =1, cex = 0.8, adj = 1.1, padj = 2.2)
+
+mtext("Depth: 0-5 cm", 3, 0.5, cex = 0.8, font = 2)
+#text(1.4, 2500, "*", cex = 3)
+boxplot((DOC)~Treatment*xx, data = x10, col = c("#143ee3bb", "#ff9b4aee"), xlab = " ", ylab = " ", xaxt = 'n', yaxt = 'n', ylim = c(0, 50),
+        at = c(1,2,4,5,7,8,10,11))
+mtext("Depth: 5-10 cm", 3, 0.5, cex = 0.8, font = 2)
+#text(1.4, 2500, "*", cex = 3)
 
 
+
+
+boxplot((carb)~Treatment*xx, data = x5, col = c("#143ee3bb", "#ff9b4aee"), xlab = " ", ylab = " ", xaxt = 'n', ylim = c(0, 0.2),
+        at = c(1,2,4,5,7,8,10,11), las = 1)
+mtext("C", 2, 3, outer = F, las =1, cex = 1.1, adj = 2.3)
+mtext(expression("g/cm"^3), 2, 3, outer = F, las =1, cex = 0.8, adj = 1.2, padj = 1.5)
+#text(1.4, 2500, "*", cex = 3)
+boxplot((carb)~Treatment*xx, data = x10, col = c("#143ee3bb", "#ff9b4aee"), xlab = " ", ylab = " ", xaxt = 'n', yaxt = 'n', ylim = c(0, 0.20),
+        at = c(1,2,4,5,7,8,10,11))
+#text(1.4, 2500, "*", cex = 3)
+
+
+boxplot((Cmin_c)~Treatment*xx, data = x5, col = c("#143ee3bb", "#ff9b4aee"), xlab = " ", ylab = " ", xaxt = 'n', ylim = c(0, 18),
+        at = c(4,5,7,8,10,11), xlim = c(0.5,11.5), las = 1)
+mtext("Cmin", 2, 3, outer = F, las =1, cex = 1.1)
+mtext(expression(paste(mu,"gC/hr/gc")), 2,3, las = 1, cex =0.8, adj = 0.8, padj = 2)
+#text(1.4, 2500, "*", cex = 3)
+boxplot((Cmin_c)~Treatment*xx, data = x10, col = c("#143ee3bb", "#ff9b4aee"), xlab = " ", ylab = " ", xaxt = 'n', yaxt = 'n', ylim = c(0, 18),
+        at = c(4,5,7,8,10,11), xlim = c(0.5,11.5))
+#text(1.4, 2500, "*", cex = 3)
+
+
+boxplot((Phenol)~Treatment*xx, data = x5, col = c("#143ee3bb", "#ff9b4aee"), xlab = " ", ylab = " ", xaxt = 'n', ylim = c(0, 10),
+        at = c(1,2,4,5,7,8,10,11), las = 1)
+axis(1, c(1.5, 4.5, 7.5, 10.5), c("2018\nMay", "2018\nJul", "2019\nJun", "2020\nAug"), tick = F)
+mtext("Phenol", 2, 3, outer = F, las =1, cex = 1.1, adj = 0.8)
+mtext("mg/L", 2,3, las = 1, cex =0.8, adj = 1, padj = 2)
+#text(1.4, 2500, "*", cex = 3)
+boxplot((Phenol)~Treatment*xx, data = x10, col = c("#143ee3bb", "#ff9b4aee"), xlab = " ", ylab = " ", xaxt = 'n', yaxt = 'n', ylim = c(0, 10),
+        at = c(1,2,4,5,7,8,10,11))
+#text(1.4, 2500, "*", cex = 3)
+
+axis(1, c(1.5, 4.5, 7.5, 10.5), c("2018\nMay", "2018\nJul", "2019\nJun", "2020\nAug"), tick = F)
+
+
+
+
+
+
+
+
+
+
+### the proportion of DOC that is phenolic is decreasing over time
+
+# boxplot((Phenol/DOC)~Treatment*xx, data = x5, col = c("#143ee3bb", "#ff9b4aee"), xlab = " ", ylab = " ", xaxt = 'n', ylim = c(0, 0.5),
+#         at = c(1,2,4,5,7,8,10,11), las = 1)
+# axis(1, c(1.5, 4.5, 7.5, 10.5), c("2018\nMay", "2018\nJul", "2019\nJun", "2020\nAug"), tick = F)
+# mtext("Phenol", 2, 3, outer = F, las =1, cex = 1.1, adj = 0.8)
+# mtext("mg/L", 2,3, las = 1, cex =0.8, adj = 1, padj = 2)
+# #text(1.4, 2500, "*", cex = 3)
+# boxplot((Phenol/DOC)~Treatment*xx, data = x10, col = c("#143ee3bb", "#ff9b4aee"), xlab = " ", ylab = " ", xaxt = 'n', yaxt = 'n', ylim = c(0, 0.5),
+#         at = c(1,2,4,5,7,8,10,11))
+
+
+     
