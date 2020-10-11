cb52cd7e1c793706383b051102d4e1ccab8b8d5d
Emily Ury <ury.emily@gmail.com>
2020-10-11 15:14:19 -0400

    axis labels

--- Figure3.R
+++ Figure3.R
@@ -23,20 +23,44 @@ c <- x[which(x$Site == "5"),]
 
 par(mfrow = c(4,3),  oma = c(3,7,3,1), mar = c(0.4,0.2,0.4,0.2))
 plot(a$Month, a$Cl, ylim = c(0,2900), xaxt = 'n', pch = c(19,21)[a$Depth], col = c("#143ee3bb", "#ff9b4aee")[a$Treatment])
+abline(v=0, lty =2)
+mtext("Cl-", 2, 3, outer = F, las =1, cex = 1.1, adj = 1.6)
+mtext("ug/g soil", 2, 3, outer = F, las =1, cex = 0.8, adj = 1, padj = 2)
+mtext("Dry", 3, 1, cex = 1.2)
 plot(b$Month, b$Cl, ylim = c(0,2900), yaxt = 'n', xaxt = 'n', pch = c(19,21)[a$Depth], col = c("#143ee3bb", "#ff9b4aee")[a$Treatment])
+abline(v=0, lty =2)
+mtext("Intermediate", 3, 1, cex = 1.2)
 plot(c$Month, c$Cl, ylim = c(0,2900), yaxt = 'n', xaxt = 'n', pch = c(19,21)[a$Depth], col = c("#143ee3bb", "#ff9b4aee")[a$Treatment])
-
+abline(v=0, lty =2)
+mtext("Wet", 3, 1, cex = 1.2)
 
 plot(a$Month, a$SM, ylim = c(0,50), xaxt = 'n', pch = c(19,21)[a$Depth], col = c("#143ee3bb", "#ff9b4aee")[a$Treatment])
+abline(v=0, lty =2)
+mtext("Soil", 2, 2, outer = F, las =1, cex = 1, adj = 1.7)
+mtext("moisture", 2, 2, outer = F, las =1, cex = 1, adj = 1, padj = 2)
+mtext("%", 2, 3, outer = F, las =1, cex = 0.8, adj = 2, padj = 4)
 plot(b$Month, b$SM, ylim = c(0,50), yaxt = 'n', xaxt = 'n', pch = c(19,21)[a$Depth], col = c("#143ee3bb", "#ff9b4aee")[a$Treatment])
+abline(v=0, lty =2)
 plot(c$Month, c$SM, ylim = c(0,50), yaxt = 'n', xaxt = 'n', pch = c(19,21)[a$Depth], col = c("#143ee3bb", "#ff9b4aee")[a$Treatment])
-
+abline(v=0, lty =2)
 
 plot(a$Month, a$pH, ylim = c(3,7), xaxt = 'n', pch = c(19,21)[a$Depth], col = c("#143ee3bb", "#ff9b4aee")[a$Treatment])
+abline(v=0, lty =2)
+mtext("pH", 2, 3, outer = F, las =1, cex = 1.1, adj = 1.6)
 plot(b$Month, b$pH, ylim = c(3,7), yaxt = 'n', xaxt = 'n', pch = c(19,21)[a$Depth], col = c("#143ee3bb", "#ff9b4aee")[a$Treatment])
+abline(v=0, lty =2)
 plot(c$Month, c$pH, ylim = c(3,7), yaxt = 'n', xaxt = 'n', pch = c(19,21)[a$Depth], col = c("#143ee3bb", "#ff9b4aee")[a$Treatment])
+abline(v=0, lty =2)
+
 
 plot(a$Month, a$DOC, ylim = c(0,40), xaxt = 'n', pch = c(19,21)[a$Depth], col = c("#143ee3bb", "#ff9b4aee")[a$Treatment])
+axis(1, at = c(-11, 20, 32,46), c("2015", "2018", "2019", "2020"))
+abline(v=0, lty =2)
+mtext("DOC", 2, 1, outer = F, las =1, cex = 1.1, adj = 1.8)
+mtext("mg/L", 2, 3, outer = F, las =1, cex = 0.8, adj = 1.3, padj = 2)
 plot(b$Month, b$DOC, ylim = c(0,40), yaxt = 'n', xaxt = 'n', pch = c(19,21)[a$Depth], col = c("#143ee3bb", "#ff9b4aee")[a$Treatment])
+axis(1, at = c(-11, 20, 32,46), c("2015", "2018", "2019", "2020"))
+abline(v=0, lty =2)
 plot(c$Month, c$DOC, ylim = c(0,40), yaxt = 'n', xaxt = 'n', pch = c(19,21)[a$Depth], col = c("#143ee3bb", "#ff9b4aee")[a$Treatment])
-
+axis(1, at = c(-11, 20, 32,46), c("2015", "2018", "2019", "2020"))
+abline(v=0, lty =2)
