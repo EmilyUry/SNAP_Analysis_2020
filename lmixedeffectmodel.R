153816e80d1352cb1a87f0f9b202bfc67b847bf4
Emily Ury <ury.emily@gmail.com>
2020-10-12 21:35:52 -0400

    test other response variables

--- lmixedeffectmodel.R
+++ lmixedeffectmodel.R
@@ -14,6 +14,7 @@ names(x) <- c("Date", "Year", "Site", "Treatment", "Depth", "Core", "Salinity","
               "Cl", "SO4", "Na", "K", "Mg", "Ca", "TIC", "TCC", "NH4", "ICNO3", "ICPO4",
               "Cmin_s", "Cmin_c", "SIR_s", "SIR_c", "Br", "Phenol", "NO3", "PO4", "Suva254")
 
+x$carb <- x$BD*x$LOI/100 ## grams carbon per cubic cm soil
 
 lX<-log(x[,c(16:21, 24, 33, 34)])
 colnames(lX)<-paste("log",colnames(lX),sep="")
@@ -65,5 +66,45 @@ summary(model1)
 summary(model2)
 
 anova(model2, model3)
+
+
+model1 <- lmer(DOC~logCl + logSO4 + pH + (1|Site), data = D, REML = F)
+null1 <- lmer(DOC~1 + (1|Site), data = D, REML = F)
+anova(null1, model1)
+summary(model2)
+
+
+model1 <- lmer(DOC~logCl + logSO4 + pH + SM + (1|Site), data = D, REML = F)
+null1 <- lmer(DOC~1 + (1|Site), data = D, REML = F)
+anova(null1, model1)
+summary(model1)
+
+
+
+### carb
+model1 <- lmer(carb~logCl + logSO4 + pH + (1|Site), data = D, REML = F)
+null1 <- lmer(carb~1 + (1|Site), data = D, REML = F)
+anova(null1, model1)
+summary(model2)
+
+
+
+### Cmin
+model1 <- lmer(Cmin_c~logCl + logSO4 + pH + (1|Site), data = d, REML = F)
+null1 <- lmer(Cmin_c~1 + (1|Site), data = d, REML = F)
+anova(null1, model1)
+summary(model2)
+
+
+
+### Cmin
+model1 <- lmer(Phenol~logCl + logSO4 + pH + (1|Site), data = D, REML = F)
+null1 <- lmer(Phenol~1 + (1|Site), data = D, REML = F)
+anova(null1, model1)
+summary(model2)
+
+
+
+
 
 
