d6e30516b287afc18ee5e2f04c8883ec1f8d1753
Emily Ury <ury.emily@gmail.com>
2020-11-10 08:21:30 -0500

    combined year model created

--- reLMEmodel.R
+++ reLMEmodel.R
@@ -5,9 +5,11 @@
 
 
 library(lme4)
-install.packages("lmerTest")
+#install.packages("lmerTest")
 library(lmerTest)
 library(MuMIn)
+
+setwd("C:/Users/uryem/Dropbox (Duke Bio_Ea)/My data/SNAP_2020")
 
 
 
@@ -36,54 +38,54 @@ x$Depth <- as.factor(x$Depth)
 ## C = July 2018 (0-5), a = July 2018 (5-10)
 ## D = May 2018 (0-5), a = May 2018 (5-10)
 
-A <- x[which(x$Year == 2020 & x$Depth == "(0-5)"),]
-a <- x[which(x$Year == 2020 & x$Depth == "(5-10)"),]
-
-B <- x[which(x$Year == 2019 & x$Depth == "(0-5)"),]
-b <- x[which(x$Year == 2019 & x$Depth == "(5-10)"),]
-
-C <- x[which(x$Date == "July 10th, 2018"& x$Depth == "(0-5)"),]
-c <- x[which(x$Date == "July 10th, 2018" & x$Depth == "(5-10)"),]
-
-D <- x[which(x$Date == "May 10th, 2018" & x$Depth == "(0-5)"),]
-d <- x[which(x$Date == "May 10th, 2018" & x$Depth == "(5-10)"),]
+# A <- x[which(x$Year == 2020 & x$Depth == "(0-5)"),]
+# a <- x[which(x$Year == 2020 & x$Depth == "(5-10)"),]
+# 
+# B <- x[which(x$Year == 2019 & x$Depth == "(0-5)"),]
+# b <- x[which(x$Year == 2019 & x$Depth == "(5-10)"),]
+# 
+# C <- x[which(x$Date == "July 10th, 2018"& x$Depth == "(0-5)"),]
+# c <- x[which(x$Date == "July 10th, 2018" & x$Depth == "(5-10)"),]
+# 
+# D <- x[which(x$Date == "May 10th, 2018" & x$Depth == "(0-5)"),]
+# d <- x[which(x$Date == "May 10th, 2018" & x$Depth == "(5-10)"),]
 
 Z <- x[which(x$Depth == "(0-5)"),]
 z <- x[which(x$Depth == "(5-10)"),]
 
 
-model1 <- lmer(DOC~logCl + pH + SM + (1|Site), data = A, REML = F)
-model1 <- lmer(DOC~logCl + pH + logCl*pH + (1|Site), data = A, REML = F)
-
-
-options(na.action = "na.fail")
-dredge(model1, beta = "sd", rank = "AICc")
-
-
-
-M.A <- lmer(DOC~logCl + pH + logCl*pH + SM + (1|Site), data = A, REML = F)
-summary(M.A)
-M.a <- lmer(DOC~logCl + pH + logCl*pH + (1|Site), data = a, REML = F)
-summary(M.a)
-M.B <- lmer(DOC~logCl + pH + logCl*pH +  (1|Site), data = B, REML = F)
-summary(M.B)
-M.b <- lmer(DOC~logCl + pH + logCl*pH +  (1|Site), data = b, REML = F)
-summary(M.b)
-M.C <- lmer(DOC~logCl + pH + logCl*pH +  (1|Site), data = C, REML = F)
-summary(M.C)
-M.c <- lmer(DOC~logCl + pH + logCl*pH +  (1|Site), data = c, REML = F)
-summary(M.c)
-M.D <- lmer(DOC~logCl + pH + logCl*pH +  (1|Site), data = D, REML = F)
-summary(M.D)
-M.d <- lmer(DOC~logCl + pH + logCl*pH + (1|Site), data = d, REML = F)
-summary(M.d)
-
-
-
-
-
-M.A <- lmer(DOC~Treatment +  (1|Site), data = a, REML = T)
-summary(M.A)
+# model1 <- lmer(DOC~logCl + pH + SM + (1|Site), data = A, REML = F)
+# model1 <- lmer(DOC~logCl + pH + logCl*pH + (1|Site), data = A, REML = F)
+# 
+# 
+# options(na.action = "na.fail")
+# dredge(model1, beta = "sd", rank = "AICc")
+# 
+# 
+# 
+# M.A <- lmer(DOC~logCl + pH + logCl*pH + SM + (1|Site), data = A, REML = F)
+# summary(M.A)
+# M.a <- lmer(DOC~logCl + pH + logCl*pH + (1|Site), data = a, REML = F)
+# summary(M.a)
+# M.B <- lmer(DOC~logCl + pH + logCl*pH +  (1|Site), data = B, REML = F)
+# summary(M.B)
+# M.b <- lmer(DOC~logCl + pH + logCl*pH +  (1|Site), data = b, REML = F)
+# summary(M.b)
+# M.C <- lmer(DOC~logCl + pH + logCl*pH +  (1|Site), data = C, REML = F)
+# summary(M.C)
+# M.c <- lmer(DOC~logCl + pH + logCl*pH +  (1|Site), data = c, REML = F)
+# summary(M.c)
+# M.D <- lmer(DOC~logCl + pH + logCl*pH +  (1|Site), data = D, REML = F)
+# summary(M.D)
+# M.d <- lmer(DOC~logCl + pH + logCl*pH + (1|Site), data = d, REML = F)
+# summary(M.d)
+# 
+# 
+# 
+# 
+# 
+# M.A <- lmer(DOC~Treatment +  (1|Site), data = a, REML = T)
+# summary(M.A)
 
 
 
@@ -115,4 +117,35 @@ summary(Mz)
 
 
 
+
+
+MZ <- lmer(DOC~ logCl*pH + (1 | Site/Date) , data = Z, REML = F)
+Mz <- lmer(DOC~logCl + logCl*pH + (1 | Site/Date), data = z, REML = F)
+summary(MZ)
+summary(Mz)
+
+
+MZ <- lmer(carb~logCl + pH + logCl*pH + (1 | Site/Date) , data = Z, REML = F)
+Mz <- lmer(carb~logCl + pH + logCl*pH + (1 | Site/Date), data = z, REML = F)
+summary(MZ)
+summary(Mz)
+
+options(na.action = "na.omit")
+MZ <- lmer(Cmin_c~logCl + pH + logCl*pH + (1 | Site/Date) , data = Z, REML = F)
+Mz <- lmer(Cmin_c~logCl + pH + logCl*pH + (1 | Site/Date), data = z, REML = F)
+summary(MZ)
+summary(Mz)
+
+
+MZ <- lmer(Phenol~logCl + pH + logCl*pH + (1 | Site/Date) , data = Z, REML = F)
+Mz <- lmer(Phenol~logCl + pH + logCl*pH + (1 | Site/Date), data = z, REML = F)
+summary(MZ)
+summary(Mz)
+
+
 ### note in the above that the variance term of the random slope Date is very high
+
+
+
+
+plot(x$Cl, x$DOC, col = )
