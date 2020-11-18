c64ba722aa961563c034396c4c58ef1b9f5bf8be
Emily Ury <ury.emily@gmail.com>
2020-11-18 11:55:35 -0500

    init

---
+++ pH_facet.R
@@ -0,0 +1,26 @@
+
+
+
+
+##pH_facet_plot
+
+
+
+setwd("C:/Users/uryem/Dropbox (Duke Bio_Ea)/My data/SNAP_2020")
+
+x <- read.csv("SNAP_3year_harm.csv", head = T)
+head(x)
+names(x) <- c("Date", "Year", "Site", "Treatment", "Depth", "Core", "Salinity","Cond", "BD", 
+              "SM", "LOI", "pH", "Roots", "DOC", "TDN", 
+              "Cl", "SO4", "Na", "K", "Mg", "Ca", "TIC", "TCC", "NH4", "ICNO3", "ICPO4",
+              "Cmin_s", "Cmin_c", "SIR_s", "SIR_c", "Br", "Phenol", "NO3", "PO4", "Suva254")
+x$carb <- x$BD*x$LOI/100 ## grams carbon per cubic cm soil
+
+x$Yr_mo <- c(rep("2020",60), rep("2019", 60), rep("2018 May", 60), rep("2018 Jul", 60))
+x$xx <- c(rep(4,60), rep(3, 60), rep(1, 60), rep(2, 60))
+
+x$xxx <- as.factor(x$xx)
+
+Site1 <- x[which(x$Site == "1"),]
+Site3 <- x[which(x$Site == "3"),]
+Site5 <- x[which(x$Site == "5"),]
