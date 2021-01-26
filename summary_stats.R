1e11898ced6b752f6db3e361058af6dd3b1698c0
Emily Ury <ury.emily@gmail.com>
2021-01-25 20:02:08 -0500

    init

---
+++ summary_stats.R
@@ -0,0 +1,28 @@
+
+setwd("C:/Users/uryem/Dropbox (Duke Bio_Ea)/My data/SNAP_2020")
+
+
+x <- read.csv("SNAP_3year_harm.csv", head = T)
+head(x)
+names(x) <- c("Date", "Year", "Site", "Treatment", "Depth", "Core", "Salinity","Cond", "BD", 
+              "SM", "LOI", "pH", "Roots", "DOC", "TDN", 
+              "Cl", "SO4", "Na", "K", "Mg", "Ca", "TIC", "TCC", "NH4", "ICNO3", "ICPO4",
+              "Cmin_s", "Cmin_c", "SIR_s", "SIR_c", "Br", "Phenol", "NO3", "PO4", "Suva254")
+
+x$carb <- x$BD*x$LOI/100 ## grams carbon per cubic cm soil
+lX<-log(x[,c(16:21, 24, 33, 34)])
+colnames(lX)<-paste("log",colnames(lX),sep="")
+x<-cbind(x,lX); rm(lX)
+x$ID<-paste(x$Site,x$Treatment,x$Core,sep="")
+x$Treatment <- as.factor(x$Treatment)
+x$Site <- as.factor(x$Site)
+x$Depth <- as.factor(x$Depth)
+
+
+### CMIN
+library(plyr)
+z <- ddply(x, .(Treatment, Site, Date), summarise, 
+           cmin = mean(Cmin_c, na.rm = T),
+           se.cmin = sqrt(var(Cmin_c, na.rm = T))/length(Cmin_c))
+names(z) <- c("Treatment", "Site", "Date", "cmin_avg", "cmin_se")
+z
