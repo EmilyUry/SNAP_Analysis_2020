5fca9a90eadc31f8647be7596c11401bdff5530d
Emily Ury <ury.emily@gmail.com>
2021-02-04 23:04:52 -0500

    created a boxplot by Site, date, depth, treatment

--- newFigures.R
+++ newFigures.R
@@ -26,3 +26,28 @@ names(x) <- c("Date", "Year", "Site", "Treatment", "Depth", "Core", "Salinity","
 ## CMin
 ## LOI
 # SIR
+
+labs <- c("Dry", "Int." , "Wet")
+names(labs) <- c("1","3", "5")
+
+laby <- c("Depth: 0-5 cm", "Depth: 5-10 cm")
+names(laby) <- c("(0-5)", "(5-10)")
+
+data <- x
+data$Site <- as.factor(data$Site)
+data$Date <- as.factor(data$Date)
+data$Date <- factor(data$Date , levels=c("May 10th, 2018", "July 10th, 2018", "June 20th, 2019", "August 8th, 2020"))
+date <- c(rep("Aug20", 60) , rep("Jun19", 60) , rep("May18", 60), rep("Jul18", 60))
+data$date <- factor(date, levels = c("May18", "Jul18", "Jun19", "Aug20"))
+data$Depth <- as.factor(data$Depth)
+data$Treatment <- as.factor(data$Treatment)
+data$response <- data$pH
+
+ggplot(data=data, aes(x=date, y = response, fill = Treatment)) + 
+  geom_boxplot() + 
+  facet_grid(Depth ~ Site, labeller = labeller(Site = labs, Depth = laby)) +
+  theme_bw() +
+  xlab(" ") +
+  ylab("pH") + 
+  theme(legend.position = "none") +
+  scale_fill_manual(values=c("#FFFFFF", "#db351f"), labels = c("Control", "Salt"))
