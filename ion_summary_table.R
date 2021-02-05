72d8241d12e93acdac66fec7c0765aa36071f3a2
Emily Ury <ury.emily@gmail.com>
2021-02-04 23:05:32 -0500

    made an ion table by date and treatment

--- ion_summary_table.R
+++ ion_summary_table.R
@@ -18,8 +18,8 @@ x$Mg[x$Mg < 0] <- 0.005
 x$Ca[x$Ca < 0] <- 0.005
 
 ## reorder and select variables
-x <- subset(x, select = c("Date", "Site", "Treatment", "Depth", "Cl", "SO4", "Na", "K", "Mg", "Ca"))
-
+x <- subset(x, select = c("Date", "Site", "Treatment", "Depth", "Cl", "SO4", "Na", "K", "Mg", "Ca", "TIC"))
+y <- x
 x <- x[which(x$Date == "August 8th, 2020" | x$Date == "May 10th, 2018"),]
 x <- data.table(x)
 
@@ -28,3 +28,13 @@ x$response <- x$Mg
 
 output <- x[,.("mean" = mean(response), "se" = std.error(response)),
             by = c("Date", "Treatment")]
+
+y <- data.table(y)
+y$response <- y$Cl+ y$SO4 + y$Na + y$K + y$Mg + y$Ca
+
+output <- y[,.("mean" = mean(response), "se" = std.error(response)),
+            by = c("Date", "Treatment")]
+output$mean <- round(output$mean, 5)
+
+
+output <- y[,.("mean" = mean(response), "se" = std.error(response)), by = c("Date", "Site", "Treatment", "Depth")]
