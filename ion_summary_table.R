2fa44d7d755c7d69d950432c940e160297bc9ec7
Emily Ury <ury.emily@gmail.com>
2021-02-04 20:57:46 -0500

    init

---
+++ ion_summary_table.R
@@ -0,0 +1,30 @@
+
+
+
+## ion summary table
+
+setwd("C:/Users/uryem/Dropbox (Duke Bio_Ea)/My data/SNAP_2020")
+library(data.table)
+library(plotrix)
+
+x <- read.csv("SNAP_3year_harm.csv", head = T)
+names(x) <- c("Date", "Year", "Site", "Treatment", "Depth", "Core", "Salinity","Cond", "BD", 
+              "SM", "LOI", "pH", "Roots", "DOC", "TDN", 
+              "Cl", "SO4", "Na", "K", "Mg", "Ca", "TIC", "TCC", "NH4", "ICNO3", "ICPO4",
+              "Cmin_s", "Cmin_c", "SIR_s", "SIR_c", "Br", "Phenol", "NO3", "PO4", "Suva254")
+x$Mg[is.na(x$Mg)] <- 0.005
+x$Ca[is.na(x$Ca)] <- 0.005
+x$Mg[x$Mg < 0] <- 0.005
+x$Ca[x$Ca < 0] <- 0.005
+
+## reorder and select variables
+x <- subset(x, select = c("Date", "Site", "Treatment", "Depth", "Cl", "SO4", "Na", "K", "Mg", "Ca"))
+
+x <- x[which(x$Date == "August 8th, 2020" | x$Date == "May 10th, 2018"),]
+x <- data.table(x)
+
+x$response <- x$Cl
+x$response <- x$Mg
+
+output <- x[,.("mean" = mean(response), "se" = std.error(response)),
+            by = c("Date", "Treatment")]
